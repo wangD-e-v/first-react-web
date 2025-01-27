20f5efb98624b10c26350170b3f62191fa0b2b50
Joshua A. <wang.dev6@gmail.com>
2025-01-27 14:53:52 +0800

    react-web

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <div>
+    <h1 className="text-3xl font-bold underline text-red-500">
+    Hello World!
+  </h1>
+    </div>
+
+    </>
+  )
+
+}
+
+export default App 
